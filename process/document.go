a30a781730d489b34d71680badf55ce2cc17e8a7
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-09 23:18:38 +0330

    document initialized

--- process/document.go
+++ process/document.go
@@ -3,6 +3,7 @@ package process
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"github.com/hamed-yousefi/gowl"
 )
 
 type Document struct {
@@ -15,4 +16,16 @@ func (d *Document) Start() error {
 	hash.Write(nil)
 	d.hash = base64.URLEncoding.EncodeToString(hash.Sum(nil))
 	return nil
+}
+
+func (d *Document) Name() string {
+	return "hashing-process"
+}
+
+func (d *Document) PID() gowl.PID {
+	return "p-1"
+}
+
+func (d *Document) Hash() string {
+	return d.hash
 }
