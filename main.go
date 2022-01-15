02da2c347d15a35bdf37512b83bd6f855fbd09e5
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-15 20:00:25 +0330

    simple command array added

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("./script.sh")
-	cmd.Stdout = os.Stdout
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
+	operations := []string{"login", "database", "logout"}
+
+	for _, operation := range operations {
+		log.Println(operation)
+		cmd := exec.Command("./script.sh")
+		cmd.Stdout = os.Stdout
+		err := cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Just ran subprocess %d, exiting\n", cmd.Process.Pid)
+		time.Sleep(5 * time.Second)
 	}
-	log.Printf("Just ran subprocess %d, exiting\n", cmd.Process.Pid)
-	time.Sleep(5 * time.Second)
 }
