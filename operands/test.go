20d29e19ec1e199ddd1587aac645070977bf1b1a
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-21 10:40:01 +0330

    run test updated

--- operands/test.go
+++ operands/test.go
@@ -1,21 +1,9 @@
 package operands
 
 import (
-	"log"
-	"os"
-	"os/exec"
-	"time"
+	"PM/internal/commandor"
 )
 
-var DIR = "./scripts/"
-
 func RunTest() {
-	cmd := exec.Command(DIR + "script.sh")
-	cmd.Stdout = os.Stdout
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Just ran subprocess %d, exiting\n", cmd.Process.Pid)
-	time.Sleep(2 * time.Second)
+	commandor.Exec("script.sh")
 }
