dc8da7bc7d7ea30060ce4af80c24a238760b5a89
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-12 18:51:58 +0330

    kill process added

--- storage.go
+++ storage.go
@@ -13,3 +13,13 @@ func (s *Storage) Add(p Process) {
 	s.list = append(s.list, p)
 	go p.Run()
 }
+
+func (s *Storage) Kill(ID int32) {
+	for index := 0; index < len(s.list); index++ {
+		if proc := s.list[index]; ID == proc.PID {
+			proc.Terminate = true
+			s.list = append(s.list[:index], s.list[index+1:]...)
+			break
+		}
+	}
+}
