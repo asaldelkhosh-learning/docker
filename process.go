7c8b10f39281829e97d18750164e46037e500838
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-12 18:14:20 +0330

    process functionality added

--- process.go
+++ process.go
@@ -1,11 +1,30 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Process struct {
 	PID       int32
 	Task      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	Delay     int32
 	Terminate bool
+}
+
+func (p Process) Run() {
+	// Function
+	for p.Terminate {
+		// Lock
+
+		// Do
+		fmt.Printf("Process %d is running now: %s\n", p.PID, p.Task)
+
+		// Unlock
+
+		// Waiting
+		time.Sleep(time.Second * time.Duration(p.Delay))
+	}
 }
