1399a4e810a42f4af52f070dbd5bb3da1844f550
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-12 18:34:48 +0330

    user input

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	if reader == nil {
+		panic(fmt.Errorf("problem in reader"))
+	}
+
 	Init()
+
 	for true {
-		cmd := "" // Get input
+		cmd, _ := reader.ReadString('\n')
+		cmd = strings.Trim(cmd, "\n")
+
 		switch {
 		case cmd == "new":
 			// New process function
