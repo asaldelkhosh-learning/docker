b83277838951d6a154f83165f8de63e13e0ea044
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-15 12:24:34 +0330

    two basic errors for decoder

--- input/input.go
+++ input/input.go
@@ -26,17 +26,23 @@ func (i Input) Get() string {
 	return cmd
 }
 
-func (i Input) Decode(cmd string) map[string]string {
+func (i Input) Decode(cmd string) (map[string]string, error) {
 	pack := make(map[string]string)
 	parts := strings.Split(cmd, " ")
 
+	if len(parts) == 0 {
+		return nil, fmt.Errorf("empty input")
+	}
+
 	pack["command"] = parts[0]
 
-	for index := 1; index < len(parts); index++ {
+	for index := 1; index < len(parts); index += 2 {
 		if strings.HasPrefix(parts[index], "--") {
 			pack[parts[index]] = parts[index+1]
+		} else {
+			return nil, fmt.Errorf("given flag '%s' not supported", parts[index])
 		}
 	}
 
-	return pack
+	return pack, nil
 }
