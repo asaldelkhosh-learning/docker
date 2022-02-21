be14c23a10bc0ebcb32100d908f67747524f452d
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-21 11:41:47 +0330

    process methods got extracted

---
+++ process/methods.go
@@ -0,0 +1,35 @@
+package process
+
+import (
+	"cmd/lock"
+	"fmt"
+	"time"
+)
+
+func (p *Process) Run() {
+	// Function
+	p.Called = 0
+	for !p.Terminate {
+		// Check for pause
+		if p.Pause {
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		// Lock
+		lock.C.L.Lock()
+		lock.Last = p.PID
+		// Do
+		p.Called++
+		p.UpdatedAt = time.Now()
+		// Burst
+		time.Sleep(time.Second * time.Duration(p.Burst))
+		// Unlock
+		lock.C.L.Unlock()
+		// Waiting
+		time.Sleep(time.Second * time.Duration(p.Delay))
+	}
+}
+
+func (p *Process) Status(i int) string {
+	return fmt.Sprintf("%d: Process %d | Task %s | Executed %d | Last Update %s\n", i+1, p.PID, p.Task, p.Called, p.UpdatedAt)
+}
--- process/process.go
+++ process/process.go
@@ -1,8 +1,6 @@
 package process
 
 import (
-	"cmd/lock"
-	"fmt"
 	"time"
 )
 
@@ -16,32 +14,4 @@ type Process struct {
 	Terminate bool
 	Called    int
 	Pause     bool
-}
-
-func (p *Process) Run() {
-	// Function
-	p.Called = 0
-	for !p.Terminate {
-		// Check for pause
-		if p.Pause {
-			time.Sleep(3 * time.Second)
-			continue
-		}
-		// Lock
-		lock.C.L.Lock()
-		lock.Last = p.PID
-		// Do
-		p.Called++
-		p.UpdatedAt = time.Now()
-		// Burst
-		time.Sleep(time.Second * time.Duration(p.Burst))
-		// Unlock
-		lock.C.L.Unlock()
-		// Waiting
-		time.Sleep(time.Second * time.Duration(p.Delay))
-	}
-}
-
-func (p *Process) Status(i int) string {
-	return fmt.Sprintf("%d: Process %d | Task %s | Executed %d | Last Update %s\n", i+1, p.PID, p.Task, p.Called, p.UpdatedAt)
 }
