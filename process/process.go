3d47cff3f4be94493086425b0a540d4f9b4372d3
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-25 00:05:40 +0430

    fix: procces

--- process/process.go
+++ process/process.go
@@ -1,7 +1,10 @@
 package process
 
 import (
+	"fmt"
 	"time"
+
+	"github.com/amirhnajafiz/procces-monitoring/lock"
 )
 
 type Process struct {
@@ -14,4 +17,32 @@ type Process struct {
 	Terminate bool
 	Called    int
 	Pause     bool
+}
+
+func (p *Process) Run() {
+	// Function
+	p.Called = 0
+	for !p.Terminate {
+		// Check for pause
+		if p.Pause {
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		// Lock
+		lock.C.L.Lock()
+		lock.Last = p.PID
+		// Do
+		p.Called++
+		p.UpdatedAt = time.Now()
+		// Burst
+		time.Sleep(time.Second * time.Duration(p.Burst))
+		// Unlock
+		lock.C.L.Unlock()
+		// Waiting
+		time.Sleep(time.Second * time.Duration(p.Delay))
+	}
+}
+
+func (p *Process) Status(i int) string {
+	return fmt.Sprintf("%d: Process %d | Task %s | Executed %d | Last Update %s\n", i+1, p.PID, p.Task, p.Called, p.UpdatedAt)
 }
